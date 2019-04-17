3cb6c1cad8338514ce4c02a301f4de16d43d9a40
WendyG <33284819+weagale@users.noreply.github.com>
2019-04-17 14:24:56 -0400

    Update NumTitlesGT.sql
    Major re-write with Jeffrey's help and Grant to correct user for schema path.

--- AdditionalMetadataBackup/03 Views - for GT-LEAD-BloomReaderDashboard/bloomreader/Views/NumTitlesGT.sql
+++ AdditionalMetadataBackup/03 Views - for GT-LEAD-BloomReaderDashboard/bloomreader/Views/NumTitlesGT.sql
@@ -1,22 +1,15 @@
-REVOKE SELECT ON bloomreader.NumTitlesGT FROM readbloomtester;
+REVOKE SELECT ON bloomreader.NumTitlesGT FROM bloomreaderuser;
 DROP VIEW bloomreader.NumTitlesGT CASCADE;
 
 CREATE OR REPLACE VIEW bloomreader.NumTitlesGT AS
-    SELECT COUNT(*), a.title, row_number() OVER (ORDER BY a.timestamp), a.timestamp, a.Country
-	FROM (
-        SELECT COUNT(*), d.title, row_number() OVER (ORDER BY d.timestamp), d.timestamp, 
-		  (SELECT i.country_name from public.ip2loc_sm_tab AS i
-		   WHERE public.ip2int(d.context_ip) = i.context_ip
-		    GROUP BY i.country_name) AS Country,										
-           RANK() OVER (partition by d.title ORDER BY d.timestamp ASC) AS RANK
-    	FROM (SELECT * FROM bloomreader.pages_read
-				UNION ALL
-			  SELECT * FROM bloomreaderbeta.pages_read) AS d
-		WHERE d.branding_project_name = 'Juarez-Guatemala'
-        GROUP BY d.title, d.timestamp, d.country_name, d.context_ip
-		  ) AS a  
-	WHERE rank = 1 
-	GROUP BY a.title, a.timestamp, a.Country;
+    SELECT COUNT( DISTINCT (d.title))
+   	FROM (SELECT * FROM bloomreader.pages_read
+		UNION ALL
+		  SELECT * FROM bloomreaderbeta.pages_read) AS d 
+		  WHERE EXISTS (SELECT 1 FROM public.ip2location AS b 
+                   		WHERE ip2int(d.context_ip) BETWEEN b.ip_from and b.ip_to
+						AND b.country_name = 'Guatemala') 	
+				AND d.branding_project_name = 'Juarez-Guatemala';
     
-GRANT SELECT ON bloomreader.NumTitlesGT TO readbloomtester;
+GRANT SELECT ON bloomreader.NumTitlesGT TO bloomreaderuser;
 select * FROM bloomreader.NumTitlesGT;
