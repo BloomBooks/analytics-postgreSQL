5950107e2ffaa51e4716b0f80c6447e8eba304ba
WendyG <33284819+weagale@users.noreply.github.com>
2019-04-17 14:30:16 -0400

    Update TimesPerMonGT.sql
    Major re-write with Jeffrey again.

--- AdditionalMetadataBackup/03 Views - for GT-LEAD-BloomReaderDashboard/bloomreader/Views/TimesPerMonGT.sql
+++ AdditionalMetadataBackup/03 Views - for GT-LEAD-BloomReaderDashboard/bloomreader/Views/TimesPerMonGT.sql
@@ -7,14 +7,15 @@ CREATE OR REPLACE VIEW bloomreader.TimePerMonGT AS
 	FROM (SELECT * FROM bloomreader.application_opened
 		  UNION ALL
 		  SELECT * FROM bloomreaderbeta.application_opened) AS a
-		  	 INNER JOIN bloomreader.pages_read AS f ON	   	 
-		   		a.context_device_id = f.context_device_id and	         
-				f.branding_project_name = 'Juarez-Guatemala'
-	INNER JOIN public.ip2loc_sm_tab AS b ON	   	 
-		 b.country_name = 'Guatemala' and	         
-			public.ip2int(a.context_ip) = b.context_ip			
-	WHERE CAST((a.timestamp AT TIME ZONE 'AEST') AS date) > ((date_trunc('month', (CAST((current_date AT TIME ZONE 'AEST') AS date)))) - INTERVAL '11 months') 
-    GROUP BY CAST((a.timestamp AT TIME ZONE 'AEST') AS date), a.id, a.context_ip;
+		  	 INNER JOIN (SELECT x.context_device_id FROM bloomreader.pages_read AS x
+						 WHERE x.branding_project_name = 'Juarez-Guatemala'
+						UNION DISTINCT 
+						SELECT y.context_device_id FROM bloomreaderbeta.pages_read AS y
+						WHERE y.branding_project_name = 'Juarez-Guatemala')  AS f ON	   	 
+		   		a.context_device_id = f.context_device_id 	         
+	WHERE CAST((a.timestamp AT TIME ZONE 'AEST') AS date) > ((date_trunc('month', (CAST((current_date AT TIME ZONE 'AEST') AS date)))) - INTERVAL '11 months')
+	 AND CAST((a.timestamp AT TIME ZONE 'AEST') AS date) > '2019-02-26'
+	GROUP BY CAST((a.timestamp AT TIME ZONE 'AEST') AS date), a.id ;
 
 GRANT SELECT ON bloomreader.TimePerMonGT TO readbloomtester;
 GRANT SELECT ON bloomreader.TimePerMonGT TO bloomgtuser;
