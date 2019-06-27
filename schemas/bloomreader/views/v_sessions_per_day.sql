ad5fa360b99e4bec7bc09fa92875338781e120a6
John Hatton <hattonjohn@gmail.com>
2019-06-27 11:03:06 -0600

    Add branding & country to v_sessions_per_day

--- schemas/bloomreader/views/v_sessions_per_day.sql
+++ schemas/bloomreader/views/v_sessions_per_day.sql
@@ -1,10 +1,15 @@
 -- View: bloomreader.v_sessions_per_day
 
--- DROP VIEW bloomreader.v_sessions_per_day;
+DROP VIEW bloomreader.v_sessions_per_day;
+
 
 CREATE OR REPLACE VIEW bloomreader.v_sessions_per_day AS
-
-select time_local::date as date,
-    count(*) as number_sessions
+select time_local::date as date_local,
+       book_branding,
+       country,
+       count(*) as number_sessions
 from bloomreader.v_pages_read
-group by date;
+group by time_local::date,
+         book_branding,
+         country;
+
