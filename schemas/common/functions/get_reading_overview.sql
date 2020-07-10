cd78efc77ff44a4bb163655b3041a3485ad34dca
andrew-polk <andrew_polk@sil.org>
2020-07-10 15:48:04 -0700

    Add Bloom Reader read count to overview

--- schemas/common/functions/get_reading_overview.sql
+++ schemas/common/functions/get_reading_overview.sql
@@ -1,12 +1,16 @@
--- DROP FUNCTION public.get_reading_overview(BOOLEAN, DATE, DATE, TEXT, TEXT);
+-- DROP FUNCTION common.get_reading_overview(BOOLEAN, DATE, DATE, TEXT, TEXT);
 
-CREATE OR REPLACE FUNCTION public.get_reading_overview(
+CREATE OR REPLACE FUNCTION common.get_reading_overview(
         p_useBookIds BOOLEAN,
 	p_from DATE DEFAULT DATE(20000101::TEXT), 
         p_to DATE DEFAULT DATE(30000101::TEXT),
         p_branding TEXT DEFAULT NULL, 
         p_country TEXT DEFAULT NULL)
-    RETURNS TABLE (bookCount BIGINT, deviceCount BIGINT, languageCount BIGINT)
+    RETURNS TABLE (
+        bookCount BIGINT, 
+        deviceCount BIGINT, 
+        languageCount BIGINT,
+        bloomReaderReadCount BIGINT)
 AS $$
 
 DECLARE
@@ -20,7 +24,8 @@ THEN
         -- This works on BR and BR Beta, but not BLORG.
         SELECT  COUNT(DISTINCT r.book_instance_id) AS bookCount,
                 COUNT(DISTINCT r.device_unique_id) AS deviceCount,
-                COUNT(DISTINCT r.book_language_code) AS languageCount
+                COUNT(DISTINCT r.book_language_code) AS languageCount,
+                COUNT(*) AS bloomReaderReadCount
         FROM    bloomreader.v_pages_read r,
                 temp_book_ids b
         WHERE   r.book_instance_id = b.book_instance_id AND
@@ -34,7 +39,8 @@ ELSE
         -- This works on BR and BR Beta, but not BLORG.
         SELECT  COUNT(DISTINCT book_instance_id) AS bookCount,
                 COUNT(DISTINCT device_unique_id) AS deviceCount,
-                COUNT(DISTINCT book_language_code) AS languageCount
+                COUNT(DISTINCT book_language_code) AS languageCount,
+                COUNT(*) AS bloomReaderReadCount
         FROM    bloomreader.v_pages_read r
         WHERE   r.book_branding = p_branding AND
                 r.country = p_country AND        
