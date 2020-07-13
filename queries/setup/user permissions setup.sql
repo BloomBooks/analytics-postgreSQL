2cb3d2f8ff8a4a7ee8b96186cee9fa4311062d16
Jeffrey Su <jeffrey_su@sil.org>
2020-07-13 08:55:58 -0500

    Commit missing user permissions setup changes

--- queries/setup/user permissions setup.sql
+++ queries/setup/user permissions setup.sql
@@ -27,6 +27,7 @@ GRANT USAGE ON SCHEMA public TO stats;
 GRANT SELECT ON TABLE common.mv_reading_perbook_events TO stats;
 GRANT SELECT ON TABLE common.mv_reading_perday_events TO stats;
 GRANT SELECT ON TABLE common.mv_reading_perday_events_by_branding_and_country TO stats;
+GRANT SELECT ON TABLE common.mv_comprehension TO stats;
 GRANT SELECT ON TABLE bloomlibrary_org.v_download_book to stats;
 GRANT SELECT ON TABLE bloomlibrary_org.v_book_detail to stats;
 GRANT USAGE ON SCHEMA bloomreader TO stats;
