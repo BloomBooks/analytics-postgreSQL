ec0ef005d45eb84d65d31ba30a04ec298f0235a5
Andrew Polk <andrew_polk@sil.org>
2021-10-26 13:34:14 -0700

    Add v_pages_read_efl

---
+++ schemas/common/views/v_pages_read_efl.sql
@@ -0,0 +1,13 @@
+-- View: common.v_pages_read_efl
+
+-- DROP VIEW common.v_pages_read_efl;
+
+CREATE OR REPLACE VIEW common.v_pages_read_efl AS
+
+SELECT  *
+FROM    common.mv_pages_read
+WHERE   book_branding = 'Education-For-Life'
+;
+
+GRANT SELECT ON TABLE common.v_pages_read_efl TO read_education_for_life;
+GRANT SELECT ON TABLE common.v_pages_read_efl TO readbloom;
