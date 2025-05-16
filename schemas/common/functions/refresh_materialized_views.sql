4b06f2df556e0d8d837f1064aba61dae232b7000
Andrew Polk <andrew_polk@sil.org>
2025-05-16 10:35:17 -0700

    Take 2: refresh materialized views in transactions (BL-14672)

--- schemas/common/functions/refresh_materialized_views.sql
+++ schemas/common/functions/refresh_materialized_views.sql
@@ -1,31 +1,39 @@
--- FUNCTION: common.refresh_materialized_views()
+-- PROCEDURE: common.refresh_materialized_views
 
--- DROP FUNCTION common.refresh_materialized_views();
+-- DROP PROCEDURE common.refresh_materialized_views;
 
-CREATE OR REPLACE FUNCTION common.refresh_materialized_views()
-RETURNS void
+CREATE OR REPLACE PROCEDURE common.refresh_materialized_views()
+LANGUAGE plpgsql
 AS $$
 BEGIN
-    PERFORM common.refresh_mv_in_transaction('common.mv_book_detail');
-    PERFORM common.refresh_mv_in_transaction('common.mv_download_book');
-    -- Some of the later views in this list use mv_pages_read, 
-    -- so it needs to get refreshed before them.
-    PERFORM common.refresh_mv_in_transaction('common.mv_pages_read');
-    PERFORM common.refresh_mv_in_transaction('common.mv_comprehension');
-    PERFORM common.refresh_mv_in_transaction('common.mv_reading_perbook_events');
-    PERFORM common.refresh_mv_in_transaction('common.mv_reading_perday_events');
-    PERFORM common.refresh_mv_in_transaction('common.mv_reading_perday_events_by_branding_and_country');
-END; $$
-LANGUAGE 'plpgsql';
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_book_detail');
+    END;
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_download_book');
+    END;
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_pages_read');
+    END;
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_comprehension');
+    END;
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_reading_perbook_events');
+    END;
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_reading_perday_events');
+    END;
+    BEGIN
+        CALL common.refresh_mv_in_transaction('common.mv_reading_perday_events_by_branding_and_country');
+    END;
+END; $$;
 
--- Helper function that executes each refresh in its own transaction.
--- This means less total blocking time. And we hope it resolves deadlocks we were getting.    
-CREATE OR REPLACE FUNCTION common.refresh_mv_in_transaction(view_name text)
-RETURNS void
+-- Helper procedure that executes each refresh in its own transaction.
+CREATE OR REPLACE PROCEDURE common.refresh_mv_in_transaction(view_name TEXT)
+LANGUAGE plpgsql
 AS $$
 BEGIN
     EXECUTE format('REFRESH MATERIALIZED VIEW %s', view_name);
-END; $$
-LANGUAGE 'plpgsql'
--- This is the key setting that makes each call run in a new transaction
-SECURITY DEFINER;
+    COMMIT;
+END; $$;
